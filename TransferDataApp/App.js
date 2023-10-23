137b9af84800d6f040ad8ec1adac61db59e64640
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2023-10-23 22:11:02 +0200

    Prepered states for fetching data

--- TransferDataApp/App.js
+++ TransferDataApp/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
-export default function App() {
+const movieURL = "https://mocki.io/v1/fc1a7bce-07ac-4487-bad7-be6d7c47cd24";
+
+const App = () => {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [description, setDescription] = useState([]);
+
+  useEffect(() => {
+
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 48,
+  },
+  movieText: {
+    fontSize: 26,
+    fontWeight: "200",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    fontWeight: "200",
+    color: "green",
   },
 });
+
+export default App;
