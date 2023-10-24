0d2972ab122ceef325b047cca304f1ceb369237b
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2023-10-24 21:55:36 +0200

    final commit with final data fetch from fake api

--- TransferDataApp/App.js
+++ TransferDataApp/App.js
@@ -1,52 +1,47 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ActivityIndicator,
-  FlatList,
-} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const movieURL = "https://mocki.io/v1/fc1a7bce-07ac-4487-bad7-be6d7c47cd24";
 
 const App = () => {
-
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  const [title, setTitle] = useState([]);
-  const [description, setDescription] = useState([]);
+  const [quote, setQuote] = useState('');
 
   useEffect(() => {
 
-  });
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'cee3bd1e22msheedb98e87dec740p196316jsn6d22f5917037',
+        'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+      }
+    };
+
+    fetch('https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes', options)
+      .then(response => response.json())
+      .then(data => setQuote(data[0].quote))
+      .catch(error => console.error(error));
+
+  }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.quoteText}>{quote}</Text>
+    </View>
   );
+
+
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    marginTop: 48,
+    justifyContent: 'center',
+    alignItems: 'center,',
   },
-  movieText: {
-    fontSize: 26,
-    fontWeight: "200",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-  description: {
-    textAlign: "center",
-    marginBottom: 18,
-    fontWeight: "200",
-    color: "green",
+  quoteText: {
+    fontSize: 24,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    marginHorizontal: 20,
   },
 });
 
